9dbb2a61f5cbf189c3ff97cab6a1ce741e3b9e96
h <andvice@naver.com>
2017-12-13 15:12:41 +0900

    테스트 파일입니다

--- test_git/src/kr/co/sist/test/SeungWon.java
+++ test_git/src/kr/co/sist/test/SeungWon.java
@@ -4,10 +4,7 @@ public class SeungWon {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("오승원 클래스 입니다");
-		System.out.println("branch에서 올라온 파일입니다(14:37)");
-		System.out.println("마지막 테스트용 파일입니다.");
-		System.out.println("final 파일입니다.");
+		System.out.println("이것은 branch 파일입니다.");
 	}
 
 }
