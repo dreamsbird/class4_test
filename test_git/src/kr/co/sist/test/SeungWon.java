b9330b69c9dae9870592181cadb6da1cb20be274
h <andvice@naver.com>
2017-12-13 14:31:31 +0900

    branch에서 띄어쓰기를 한 파일입니다.

--- test_git/src/kr/co/sist/test/SeungWon.java
+++ test_git/src/kr/co/sist/test/SeungWon.java
@@ -5,7 +5,7 @@ public class SeungWon {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("오승원 클래스 입니다");
-		System.out.println("oswbranch에서 수정되었습니다.");
+		System.out.println("oswbranch에서 수정되었습니다. ");
 	}
 
 }
