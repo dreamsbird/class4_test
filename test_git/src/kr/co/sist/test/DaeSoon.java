3ca9ac32a47caf7be809fbf34c4ea1b870521c1d
user <user@user-PC>
2017-12-13 14:33:07 +0900

    123

--- test_git/src/kr/co/sist/test/DaeSoon.java
+++ test_git/src/kr/co/sist/test/DaeSoon.java
@@ -4,7 +4,7 @@ public class DaeSoon {
 
 	public static void main(String[] args) {
 		System.out.println("SoonDaeKukRice");  
-		System.out.println("홍기야 요강좀 가져다줘!!"); 
+		System.out.println("홍기야 요강좀 가져다줘!!");   
 	}//main
 }//class
  
