31b370a521afa9352e1adfdd3f34f1a61e802771
user <user@user-PC>
2017-12-13 14:31:24 +0900

    레드홍기

--- test_git/src/kr/co/sist/test/DaeSoon.java
+++ test_git/src/kr/co/sist/test/DaeSoon.java
@@ -3,7 +3,7 @@ package kr.co.sist.test;
 public class DaeSoon {
 
 	public static void main(String[] args) {
-		System.out.println("SoonDaeKukRice");
+		System.out.println("SoonDaeKukRice"); 
 		System.out.println("홍기야 요강좀 가져다줘!!");
 	}//main
 }//class
