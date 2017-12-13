65ba53910bdac486f7954a6137e1409374feca5b
h <andvice@naver.com>
2017-12-13 14:43:17 +0900

    final merge

--- test_git/src/kr/co/sist/test/SeungWon.java
+++ test_git/src/kr/co/sist/test/SeungWon.java
@@ -5,6 +5,8 @@ public class SeungWon {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("오승원 클래스 입니다");
+		System.out.println("branch에서 올라온 파일입니다(14:37)");
+		System.out.println("마지막 테스트용 파일입니다.");
 	}
 
 }
