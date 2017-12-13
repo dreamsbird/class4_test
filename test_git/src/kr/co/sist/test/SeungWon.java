f462eeb39893d53341624c727726e38b2fdfc19d
h <andvice@naver.com>
2017-12-13 14:13:57 +0900

    오승원이 올린 클래스 파일입니다.

---
+++ test_git/src/kr/co/sist/test/SeungWon.java
@@ -0,0 +1,10 @@
+package kr.co.sist.test;
+
+public class SeungWon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("오승원 클래스 입니다");
+	}
+
+}
